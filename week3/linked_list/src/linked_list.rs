b38ca8c269bc7cf3d69135843c7fb97053c37e42
yucheng_xiang <846735552@qq.com>
2026-05-17 09:46:59 +0800

    week3 linked_list
    Signed-off-by: yucheng_xiang <846735552@qq.com>

--- week3/linked_list/src/linked_list.rs
+++ week3/linked_list/src/linked_list.rs
@@ -1,60 +1,70 @@
 use std::fmt;
 use std::option::Option;
 
-pub struct LinkedList {
-    head: Option<Box<Node>>,
+pub struct LinkedList<T> {
+    head: Option<Box<Node<T>>>,
     size: usize,
 }
 
-struct Node {
-    value: u32,
-    next: Option<Box<Node>>,
+struct Node<T> {
+    value: T,
+    next: Option<Box<Node<T>>>,
 }
 
-impl Node {
-    pub fn new(value: u32, next: Option<Box<Node>>) -> Node {
-        Node {value: value, next: next}
+impl<T> Node<T> {
+    pub fn new(value: T, next: Option<Box<Node<T>>>) -> Node<T> {
+        Node {
+            value: value,
+            next: next,
+        }
+    }
+}
+pub trait ComputeNorm {
+    fn compute_norm(&self) -> f64 {
+        0.0
     }
 }
 
-impl LinkedList {
-    pub fn new() -> LinkedList {
-        LinkedList {head: None, size: 0}
+impl<T> LinkedList<T> {
+    pub fn new() -> LinkedList<T> {
+        LinkedList {
+            head: None,
+            size: 0,
+        }
     }
-    
+
     pub fn get_size(&self) -> usize {
         self.size
     }
-    
+
     pub fn is_empty(&self) -> bool {
         self.get_size() == 0
     }
-    
-    pub fn push_front(&mut self, value: u32) {
-        let new_node: Box<Node> = Box::new(Node::new(value, self.head.take()));
+
+    pub fn push_front(&mut self, value: T) {
+        let new_node: Box<Node<T>> = Box::new(Node::new(value, self.head.take()));
         self.head = Some(new_node);
         self.size += 1;
     }
-    
-    pub fn pop_front(&mut self) -> Option<u32> {
-        let node: Box<Node> = self.head.take()?;
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        let node: Box<Node<T>> = self.head.take()?;
         self.head = node.next;
         self.size -= 1;
         Some(node.value)
     }
 }
 
-
-impl fmt::Display for LinkedList {
+impl<T: std::fmt::Debug> fmt::Display for LinkedList<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut current: &Option<Box<Node>> = &self.head;
+        let mut current: &Option<Box<Node<T>>> = &self.head;
         let mut result = String::new();
         loop {
             match current {
                 Some(node) => {
-                    result = format!("{} {}", result, node.value);
+                    result = format!("{} {:?}", result, node.value);
                     current = &node.next;
-                },
+                }
                 None => break,
             }
         }
@@ -62,7 +72,7 @@ impl fmt::Display for LinkedList {
     }
 }
 
-impl Drop for LinkedList {
+impl<T> Drop for LinkedList<T> {
     fn drop(&mut self) {
         let mut current = self.head.take();
         while let Some(mut node) = current {
@@ -71,5 +81,61 @@ impl Drop for LinkedList {
     }
 }
 
+impl<T: Clone> Clone for LinkedList<T> {
+    fn clone(&self) -> Self {
+        Self {
+            head: self.head.as_ref().map(|node| Box::new(clone_node(node))),
+            size: self.size,
+        }
+    }
+}
 
+fn clone_node<T: Clone>(node: &Node<T>) -> Node<T> {
+    Node {
+        value: node.value.clone(),
+        next: node.next.as_ref().map(|n| Box::new(clone_node(n))),
+    }
+}
 
+impl<T: std::cmp::PartialEq> PartialEq for LinkedList<T> {
+    fn eq(&self, other: &LinkedList<T>) -> bool {
+        if self.get_size() != other.get_size() {
+            return false;
+        }
+
+        let mut curr = &self.head;
+        let mut other_curr = &other.head;
+
+        loop {
+            match (curr, other_curr) {
+                (Some(a), Some(b)) => {
+                    if a.value != b.value {
+                        return false;
+                    }
+                    curr = &a.next;
+                    other_curr = &b.next;
+                }
+                (None, None) => return true,
+                _ => return false,
+            }
+        }
+    }
+}
+
+impl<T> ComputeNorm for LinkedList<T>
+where
+    T: Into<f64> + Copy,
+{
+    fn compute_norm(&self) -> f64 {
+        let mut ret = 0.0;
+        let mut current = &self.head;
+
+        while let Some(node) = current {
+            let val: f64 = node.value.into();
+            ret += val * val;
+            current = &node.next;
+        }
+
+        ret.sqrt()
+    }
+}
--- week3/linked_list/src/main.rs
+++ week3/linked_list/src/main.rs
@@ -1,22 +1,81 @@
-use linked_list::LinkedList;
+use linked_list::{ComputeNorm, LinkedList};
+
 pub mod linked_list;
 
+fn test_clone() {
+    let mut original = LinkedList::new();
+    original.push_front(3);
+    original.push_front(2);
+    original.push_front(1);
+
+    let mut cloned = original.clone();
+
+    assert!(original == cloned);
+    assert_eq!(original.get_size(), 3);
+    assert_eq!(cloned.get_size(), 3);
+
+    cloned.pop_front();
+    assert_eq!(cloned.get_size(), 2);
+    assert_eq!(original.get_size(), 3);
+    assert!(original != cloned);
+}
+
+fn test_partial_eq() {
+    let empty_a = LinkedList::<i32>::new();
+    let empty_b = LinkedList::new();
+    assert!(empty_a == empty_b);
+
+    let mut a = LinkedList::new();
+    a.push_front(2);
+    a.push_front(1);
+
+    let mut b = LinkedList::new();
+    b.push_front(2);
+    b.push_front(1);
+    assert!(a == b);
+
+    let mut c = LinkedList::new();
+    c.push_front(2);
+    c.push_front(99);
+    assert!(a != c);
+
+    let mut shorter = LinkedList::new();
+    shorter.push_front(1);
+    assert!(a != shorter);
+}
+
+fn test_compute_norm() {
+    let empty = LinkedList::<i32>::new();
+    assert_eq!(empty.compute_norm(), 0.0);
+
+    let mut single = LinkedList::new();
+    single.push_front(5);
+    assert_eq!(single.compute_norm(), 5.0);
+
+    let mut triple = LinkedList::new();
+    triple.push_front(5);
+    triple.push_front(12);
+    triple.push_front(0);
+    assert!((triple.compute_norm() - 13.0).abs() < f64::EPSILON);
+}
+
 fn main() {
+    test_clone();
+    test_partial_eq();
+    test_compute_norm();
+    println!("Clone, PartialEq, and ComputeNorm tests passed");
+
     let mut list: LinkedList<u32> = LinkedList::new();
     assert!(list.is_empty());
     assert_eq!(list.get_size(), 0);
     for i in 1..12 {
         list.push_front(i);
     }
+
     println!("{}", list);
     println!("list size: {}", list.get_size());
     println!("top element: {}", list.pop_front().unwrap());
     println!("{}", list);
     println!("size: {}", list.get_size());
     println!("{}", list.to_string()); // ToString impl for anything impl Display
-
-    // If you implement iterator trait:
-    //for val in &list {
-    //    println!("{}", val);
-    //}
 }
